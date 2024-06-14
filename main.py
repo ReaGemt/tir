7cccf6d6d5021f118dcd3f26cb13c2353e1755d6
ReaGemt <155527397+ReaGemt@users.noreply.github.com>
2024-06-14 23:09:37 +0300

    add all cicle code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 import random
 
-
 pygame.init()
 
 SCREEN_WIDTH = 800
@@ -12,7 +11,7 @@ pygame.display.set_caption('Игра Тир')
 icon = pygame.image.load('img/icon.png')
 pygame.display.set_icon(icon)
 
-target.img = pygame.image.load("img/target.png")
+target_img = pygame.image.load("img/target.png")
 target_widht = 80
 target_height = 80
 
@@ -27,10 +26,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    pygame.draw.rect(screen, (255, 0, 0), (target_x, target_y, target_widht, target_height))
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_widht and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH  - target_widht)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_img, (target_x, target_y))
     pygame.display.update()
-
-
 
 
 
