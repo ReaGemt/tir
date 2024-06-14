7fe3b0533f9db2bcae8ffbee67ed0616a7ab907a
ReaGemt <155527397+ReaGemt@users.noreply.github.com>
2024-06-14 23:41:54 +0300

    update

--- main.py
+++ main.py
@@ -26,9 +26,12 @@ color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 clock = pygame.time.Clock()
-
 start_time = pygame.time.get_ticks()
 game_timer = 30  # 30 секунд на игру
+def update_target_position():
+    global target_x, target_y
+    target_x = random.randint(0, SCREEN_WIDTH - target_widht)
+    target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 while running:
     screen.fill(color)
@@ -43,7 +46,7 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_widht and target_y < mouse_y < target_y + target_height:
-                target_x = random.randint(0, SCREEN_WIDTH  - target_widht)
+                target_x = random.randint(0, SCREEN_WIDTH - target_widht)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
     screen.blit(target_img, (target_x, target_y))
 
@@ -55,5 +58,10 @@ while running:
 
     if game_timer <= 0:
         running = False
+screen.fill((0, 0, 0))
+final_text = font.render(f"Final Score: {score}", True, (255, 255, 255))
+screen.blit(final_text, (SCREEN_WIDTH // 2 - final_text.get_width() // 2, SCREEN_HEIGHT // 2 - final_text.get_height() // 2))
+pygame.display.update()
+pygame.time.wait(3000)
 
 pygame.quit()
