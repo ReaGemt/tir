695aef7ced0e1c098dd12ef53daab1f2cad5af18
ReaGemt <155527397+ReaGemt@users.noreply.github.com>
2024-06-14 14:30:16 +0300

    add icon

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption('Игра Тир')
-icon = pygame.image.load('img/')
+icon = pygame.image.load('img/3548954.png')
+pygame.display.set_icon(icon)
 
 
 running = True
